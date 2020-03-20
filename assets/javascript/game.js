6a4284d448a7a426e55c164be6993195d2c73938
unknown <joe_cooler@msn.com>
2020-03-19 21:16:18 -0500

    changed images and added functional progress bars for HP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,16 @@ var enemySelected = false;
 var baseAtk = 0;
 var playerDied = false;
 var playerWin = false;
+var baseFighterHP = 0;
+var baseEnemyHP = 0;
+var fighterDmg, enemyDmg = 0;
 
 var currentFighter, currentEnemy;
 var enemiesLeft = 3;
 
 var richter = {
     id: "#richter",
-    path: "assets/images/richter.jpg",
+    path: "assets/images/richter.gif",
     name: "Richter",
     attackName: ["vampire killer", "hydro storm", "blade dash"],
     attackPower: 20,
@@ -19,7 +22,7 @@ var richter = {
 
 var alucard = {
     id: "#alucard",
-    path: "assets/images/alucard.jpg",
+    path: "assets/images/alucard.png",
     name: "Alucard",
     attackName: ["alucard sword", "soul steal", "bat form"],
     attackPower: 25,
@@ -39,13 +42,15 @@ var death = {
 
 var dracula = {
     id: "#dracula",
-    path: "assets/images/dracula.jpg",
+    path: "assets/images/dracula.gif",
     name: "Dracula",
     attackName: ["bat form", "fireball", "dragon form"],
     attackPower: 35,
     counterAttackPower: 35,
     hp: 180
 };
+
+$("hr").hide();
 
 
 
@@ -64,8 +69,9 @@ $(".game-hud").on("click", "#atk-btn", function () {
 
     var fighterAtk = rand(0, 2);
     var enemyAtk = rand(0, 2);
-    var enemyDmg = currentEnemy.counterAttackPower;
-    var fighterDmg = currentFighter.attackPower;
+
+    enemyDmg = currentEnemy.counterAttackPower;
+    fighterDmg = currentFighter.attackPower;
 
     var fighterStr = currentFighter.name + " hits " + currentEnemy.name + " with " + currentFighter.attackName[fighterAtk] + " for " + fighterDmg + " damage.";
     var enemyStr = currentEnemy.name + " counter-attacks with " + currentEnemy.attackName[enemyAtk] + " for " + enemyDmg + " damage.";
@@ -73,8 +79,14 @@ $(".game-hud").on("click", "#atk-btn", function () {
     currentFighter.hp -= enemyDmg;
     currentEnemy.hp -= fighterDmg;
 
+    updateCharacterBars();
+
+
+
+
     if (currentFighter.hp <= 0) {
         $("#fight-result").text(currentFighter.name + " has died. Game over.");
+        $("#fighter").animate({opacity: '0.0'});
         $(this).text("New Game");
         fighterSelected = false;
         updateHP();
@@ -90,8 +102,9 @@ $(".game-hud").on("click", "#atk-btn", function () {
         if (enemiesLeft === 0) {
             playerWin = true;
             $(this).text("New Game");
+            setHeaderText();
         }
-        $("#enemy").animate({opacity:'0.0'});
+        $("#enemy").animate({ opacity: '0.0' });
         //setEnemyPic("", "");
         updateHP();
         return;
@@ -102,12 +115,13 @@ $(".game-hud").on("click", "#atk-btn", function () {
 
     $("#fighter-text").text(fighterStr);
     $("#enemy-text").text(enemyStr);
+    $("hr").show();
 });
 
 /***********************************************************/
 
 $("#richter").on("click", function () {
-    if (playerDied) {
+    if (playerDied || playerWin) {
         return;
     }
     if (!fighterSelected) {
@@ -122,7 +136,7 @@ $("#richter").on("click", function () {
 });
 
 $("#alucard").on("click", function () {
-    if (playerDied) {
+    if (playerDied || playerWin) {
         return;
     }
     if (!fighterSelected) {
@@ -137,7 +151,7 @@ $("#alucard").on("click", function () {
 });
 
 $("#death").on("click", function () {
-    if (playerDied) {
+    if (playerDied || playerWin) {
         return;
     }
     if (!fighterSelected) {
@@ -152,7 +166,7 @@ $("#death").on("click", function () {
 });
 
 $("#dracula").on("click", function () {
-    if (playerDied) {
+    if (playerDied || playerWin) {
         return;
     }
     if (!fighterSelected) {
@@ -173,7 +187,7 @@ function setFighterPic(id, path) {
 }
 
 function setEnemyPic(id, path) {
-    $("#enemy").animate({opacity:'1.0'});
+    $("#enemy").animate({ opacity: '1.0' });
     $("#enemy").attr("src", path);
     $(id).attr("src", " ");
 }
@@ -184,41 +198,77 @@ function setHeaderText() {
     }
 
     if (fighterSelected && enemySelected) {
-        $(".header").text("Destroy Your Enemy!");
+        $(".header").text("Destroy Your Enemies!");
+    }
+
+    if (playerWin) {
+        $(".header").text("All Enemies Vanquished!");
     }
 
 }
 
 function setFighter(fighter) {
+    baseFighterHP = fighter.hp;
     baseAtk = fighter.attackPower;
     setFighterPic(fighter.id, fighter.path);
     fighterSelected = true;
     setHeaderText();
     $("#fighter-name").text(fighter.name);
-    $("#hp-fighter").text("HP: " + fighter.hp);
+    $("#hp-fighter-text").text("HP: " + fighter.hp);
+    $(".hp-fighter-progress").attr("aria-valuemax", fighter.hp);
     return fighter;
 }
 
 function setEnemy(enemy) {
+    baseEnemyHP = enemy.hp;
     setEnemyPic(enemy.id, enemy.path);
     enemySelected = true;
     setHeaderText();
     $("#enemy-name").text(enemy.name);
-    $("#hp-enemy").text("HP: " + enemy.hp);
+    $("#hp-enemy-text").text("HP: " + enemy.hp);
     if (enemiesLeft === 3) {
         $("#fighter-hud").append('<button id="atk-btn">Attack!</button>');
     }
     $("#fight-result").text("");
+    $(".hp-enemy-progress").attr('style', 'width: 100%');
     return enemy;
 }
 
 function updateHP() {
-    if (currentFighter.hp <= 0)
+    if (currentFighter.hp <= 0) {
         currentFighter.hp = 0;
-    if (currentEnemy.hp <= 0)
+        $(".hp-fighter-progress").attr('style', 'width: 0%');
+    }
+    if (currentEnemy.hp <= 0){
         currentEnemy.hp = 0;
-    $("#hp-fighter").text("HP: " + currentFighter.hp);
-    $("#hp-enemy").text("HP: " + currentEnemy.hp);
+        $(".hp-enemy-progress").attr('style', 'width: 0%');
+    }
+    $("#hp-fighter-text").text("HP: " + currentFighter.hp);
+    $("#hp-enemy-text").text("HP: " + currentEnemy.hp);
+}
+
+function updateCharacterBars() {
+
+    var pDmg = Math.ceil((enemyDmg * 100) / baseFighterHP);
+    var eDmg = Math.ceil((fighterDmg * 100) / baseEnemyHP);
+    var fProgVal = $(".hp-fighter-progress").attr('style');
+    var eProgVal = $(".hp-enemy-progress").attr('style');
+
+    var newStr = fProgVal.replace("width: ", "");
+
+    newStr = newStr.replace("%", "");
+    newStr = parseInt(newStr);
+    newStr = "width: " + (newStr - pDmg) + "%";
+    $(".hp-fighter-progress").attr('style', newStr);
+
+    newStr = eProgVal.replace("width: ", "");
+
+    newStr = newStr.replace("%", "");
+    newStr = parseInt(newStr);
+    newStr = "width: " + (newStr - eDmg) + "%";
+    $(".hp-enemy-progress").attr('style', newStr);
+
+
 }
 
 function rand(min, max) {
