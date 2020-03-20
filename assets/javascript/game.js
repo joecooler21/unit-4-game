ec109ea6cdd1e8eda39bef081ff9b6bf411df04c
unknown <joe_cooler@msn.com>
2020-03-19 21:39:51 -0500

    added comments for final submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -58,35 +58,41 @@ $("hr").hide();
 
 $(".game-hud").on("click", "#atk-btn", function () {
 
+    // if game over, reload page and don't run any further code
     if (playerDied || playerWin) {
         location.reload();
         return;
     }
 
+    // if no enemy selected, don't run any further code
     if (!enemySelected) {
         return;
     }
 
+    // pull a random attack name from fighter & enemy objects
     var fighterAtk = rand(0, 2);
     var enemyAtk = rand(0, 2);
 
+    // get global attack values
     enemyDmg = currentEnemy.counterAttackPower;
     fighterDmg = currentFighter.attackPower;
 
+    // ready battle text for fighter & enemy attacks
     var fighterStr = currentFighter.name + " hits " + currentEnemy.name + " with " + currentFighter.attackName[fighterAtk] + " for " + fighterDmg + " damage.";
     var enemyStr = currentEnemy.name + " counter-attacks with " + currentEnemy.attackName[enemyAtk] + " for " + enemyDmg + " damage.";
 
+    // update fighter & enemy hp values based on current attacks received
     currentFighter.hp -= enemyDmg;
     currentEnemy.hp -= fighterDmg;
 
+    // call function to update health bars based on health point values
     updateCharacterBars();
 
 
-
-
+    // if fighter dies, game's over
     if (currentFighter.hp <= 0) {
         $("#fight-result").text(currentFighter.name + " has died. Game over.");
-        $("#fighter").animate({opacity: '0.0'});
+        $("#fighter").animate({ opacity: '0.0' });
         $(this).text("New Game");
         fighterSelected = false;
         updateHP();
@@ -94,6 +100,7 @@ $(".game-hud").on("click", "#atk-btn", function () {
         return;
     }
 
+    // if enemy dies, check to see if there's any more left to fight, otherwise end game with a player victory
     if (currentEnemy.hp <= 0) {
         currentEnemy.hp = 0;
         $("#fight-result").text(currentEnemy.name + " has been defeated");
@@ -105,14 +112,17 @@ $(".game-hud").on("click", "#atk-btn", function () {
             setHeaderText();
         }
         $("#enemy").animate({ opacity: '0.0' });
-        //setEnemyPic("", "");
         updateHP();
         return;
     }
 
+    // each time the player attacks, increment their base attack power by their base attack power
     currentFighter.attackPower += baseAtk;
+
+    // display fighter & enemy hp
     updateHP();
 
+    // update the battle text display area with attack strings
     $("#fighter-text").text(fighterStr);
     $("#enemy-text").text(enemyStr);
     $("hr").show();
@@ -120,6 +130,7 @@ $(".game-hud").on("click", "#atk-btn", function () {
 
 /***********************************************************/
 
+// set game status flags on character portrait clicks and return the currently selected fighter or enemy
 $("#richter").on("click", function () {
     if (playerDied || playerWin) {
         return;
@@ -180,6 +191,7 @@ $("#dracula").on("click", function () {
     }
 });
 
+// move fighter & enemy portraits to battle area
 function setFighterPic(id, path) {
 
     $("#fighter").attr("src", path);
@@ -192,6 +204,7 @@ function setEnemyPic(id, path) {
     $(id).attr("src", " ");
 }
 
+// update header with player instructions
 function setHeaderText() {
     if (fighterSelected && !enemySelected) {
         $(".header").text("Choose Your Opponent");
@@ -206,6 +219,8 @@ function setHeaderText() {
     }
 
 }
+
+// stage figter & enemy battle area
 
 function setFighter(fighter) {
     baseFighterHP = fighter.hp;
@@ -234,19 +249,20 @@ function setEnemy(enemy) {
     return enemy;
 }
 
+// update progress bar based on current fighter & enemy hp
 function updateHP() {
     if (currentFighter.hp <= 0) {
         currentFighter.hp = 0;
         $(".hp-fighter-progress").attr('style', 'width: 0%');
     }
-    if (currentEnemy.hp <= 0){
+    if (currentEnemy.hp <= 0) {
         currentEnemy.hp = 0;
         $(".hp-enemy-progress").attr('style', 'width: 0%');
     }
     $("#hp-fighter-text").text("HP: " + currentFighter.hp);
     $("#hp-enemy-text").text("HP: " + currentEnemy.hp);
 }
-
+// calculate fighter & enemy hp percentages based on max value of progress bar and update accordingly
 function updateCharacterBars() {
 
     var pDmg = Math.ceil((enemyDmg * 100) / baseFighterHP);
@@ -271,6 +287,7 @@ function updateCharacterBars() {
 
 }
 
+// random number function
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
